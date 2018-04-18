fed47c9aa95cd47c52048d55293e2876c99af8f7
freegyp <freegyp@gmail.com>
2018-04-17 17:01:14 -0700

    Fixed solution on 2018 round 1A problem C.

--- google code jam/gcj2018r1a/C.py
+++ google code jam/gcj2018r1a/C.py
@@ -5,11 +5,10 @@ tt = int(raw_input())
 for t in xrange(1,tt+1):
 	n,p = map(int,raw_input().split())
 	p = float(p)
-	noCut,cutUpper,incs = 0.0,0.0,0.0,[]
+	noCut,incs = 0.0,[]
 	for i in xrange(n):
 		w,h = map(float,raw_input().split())
 		noCut += (w+h)*2.0
-		cutUpper += (w+h)*2.0 + 2*sqrt(w*w+h*h)
 		incs.append((2*min(w,h),2*sqrt(w*w+h*h)))
 	incs.sort()
 	start,end,i = noCut,noCut,0
@@ -17,8 +16,10 @@ for t in xrange(1,tt+1):
 		w,diag = incs[i]
 		if end>=start+w:
 			end += diag
+		elif start+w>p:
+			break
 		else:
 			preInc = end-start
 			start,end = start+w,end+diag+preInc
-	ans = min(p,cutUpper)
+	ans = min(p,end)
 	print "Case #{0}: {1}".format(t,ans)
