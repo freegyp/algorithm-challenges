4cc3900e87022562fe571d8c56b168568742dfab
freegyp <freegyp@gmail.com>
2018-12-10 11:55:23 -0800

    New Kattis solution.

--- google code jam/gcj2018r2/c.py
+++ google code jam/gcj2018r2/c.py
@@ -2,37 +2,21 @@
 tt = int(raw_input())
 
 for t in xrange(1,tt+1):
-	n = int(raw_input())
-	rows,cols = {i:{} for i in xrange(n)},{i:{} for i in xrange(n)}
-	stage,total = [],0
-	for i in xrange(n):
-		row = map(int,raw_input().split())
-		stage.append(row)
-	for i in xrange(n):
-		for j in xrange(n):
-			if stage[i][j] not in rows[i]:
-				rows[i][stage[i][j]] = 1
-			else:
-				rows[i][stage[i][j]] += 1
-	for i in xrange(n):
-		for j in xrange(n):
-			if stage[i][j]<=n and stage[i][j] not in cols[j]:
-				cols[j][stage[i][j]] = 1
-			else:
-				cols[j][stage[i][j]] += 1
-	for i in xrange(n):
-		for j in xrange(n):
-			if rows[i][stage[i][j]]>1 and cols[j][stage[i][j]]>1:
-				rows[i][stage[i][j]] -= 1
-				cols[j][stage[i][j]] -= 1
-				stage[i][j] = n+1
-				total += 1
-	for i in xrange(n):
-		for j in xrange(n):
-			if stage[i][j]<=n and rows[i][stage[i][j]]>1:
-				rows[i][stage[i][j]] -= 1
-				total += 1
-			elif stage[i][j]<=n and cols[j][stage[i][j]]>1:
-				cols[j][stage[i][j]] -= 1
-				total += 1
-	print "Case #{0}: {1}".format(t,total)
+    n = int(raw_input())
+    val2ijs = {}
+    for i in xrange(n):
+        row = map(int,raw_input().split())
+        for j in xrange(n):
+            if row[j] not in val2ijs:
+                val2ijs[row[j]] = []
+            val2ijs[row[j]].append((i,j))
+    ans = 0
+    for i in xrange(-n,n+1):
+        if i in val2ijs:
+            total,rows,cols = 0,{},{}
+            for x,y in val2ijs[i]:
+                total += 1
+                rows[x] = True
+                cols[y] = True
+            ans += total-min(len(rows),len(cols))
+    print "Case #{0}: {1}".format(t,ans)
