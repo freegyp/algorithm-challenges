b4888eb8cb60cd4355e3f68d872266efaebbf1df
freegyp <freegyp@gmail.com>
2019-12-08 14:59:02 -0800

    Commit.

--- kattis/cycleseasy.cc
+++ kattis/cycleseasy.cc
@@ -1,35 +1,56 @@
-//Not accepted yet...
-#include<iostream>
-#include<unordered_set>
-#include<cstdlib>
+#include<bits/stdc++.h>
 using namespace std;
-
+#define MOD 9901
 #define uset unordered_set
+#define umap unordered_map
+
+int safeCheck(umap<int,uset<int> > &dic,int x,int y){
+	if(dic.find(x)==dic.end())return 0;
+	if(dic[x].find(y)==dic[x].end())return 0;
+	return 1;
+}
 
 int main(){
 	int tt;cin>>tt;
 	for(int t=1;t<=tt;t++){
 		int n,k;cin>>n>>k;
-		uset<int> blacklist;
-		int *counts = (int*)malloc(sizeof(int)*(1<<n));
-		for(int i=0;i<(1<<n);i++)counts[i]=0;
+		umap<int,uset<int> > taboos;
 		for(int i=0;i<k;i++){
 			int left,right;cin>>left>>right;
-			blacklist.insert((1<<(left-1))|(1<<(right-1)));
+			left--;right--;
+			if(taboos.find(1<<left)==taboos.end())taboos[1<<left] = uset<int>();
+			taboos[1<<left].insert(1<<right);
+			if(taboos.find(1<<right)==taboos.end())taboos[1<<right] = uset<int>();
+			taboos[1<<right].insert(1<<left);
 		}
-		for(int cur=1;cur<(1<<n);cur<<=1){
-			counts[cur] = 1;
-			for(int i=1;i<cur;i++){
-				int j=i;
-				while(j>0){
-					if(blacklist.find((j&-j)|cur)==blacklist.end()){
-						counts[cur|i] += counts[i];
+		umap<int,umap<int,int> > table;
+		for(int mask=1;mask<(1<<(n-1));mask++){
+			table[mask] = umap<int,int>();
+			if(mask==(mask&-mask)){
+				table[mask][mask] = 1^safeCheck(taboos,mask,1<<(n-1));
+			}else{
+				for(int mask2 = mask;mask2>0;mask2-=mask2&-mask2){
+					int out = mask2&-mask2;
+					table[mask][out] = 0;
+					int pre = mask^out;
+					for(int mask3 = pre;mask3>0;mask3-=mask3&-mask3){
+						int preOut = mask3&-mask3;
+						if(!safeCheck(taboos,preOut,out)){
+							table[mask][out] += table[pre][preOut];
+							//table[mask][out] %= MOD;
+						}
 					}
-					j -= (j&-j);
 				}
 			}
 		}
-		cout<<"Case #"<<t<<": "<<counts[(1<<n)-1]<<endl;
+		int ans = 0;
+		for(int i=1;i<(1<<(n-1));i<<=1){
+			ans += (1^safeCheck(taboos,i,1<<(n-1)))*table[(1<<(n-1))-1][i];
+			//ans %= MOD;
+		}
+		ans /= 2;
+		ans %= MOD;
+		cout<<"Case #"<<t<<": "<<ans<<endl;
 	}
 	return 0;
 }
